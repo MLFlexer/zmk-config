063b25e2dbb6062d0f9139106c4a73f5cd1dcf4f
Malthe Mathias Mølgaard Larsen <Malthe Larsen>
2023-01-26 19:35:10 +0100

    added more codes

--- config/keycodes.h
+++ config/keycodes.h
@@ -5,3 +5,18 @@
 #define _RBRA RA(N9) //]
 #define _LCBRA RA(N7) //{
 #define _RCBRA RA(N0) //}
+#define _LT NUBS //<
+#define _GT RA(NUBS) //>
+#define _BSLH LS(N7) // /
+#define _FSLH LS(NUBS) // \
+
+#define _AT RA(N2) //@
+#define _POUND RA(N3) //£
+#define _DLR RA(N4) //$
+
+#define _QM LS(MINUS) // ?
+#define _AND LS(N6) // &
+#define _SEMI LS(COMMA) // ;
+#define _COL LS(DOT) // :
+#define _UNDR LS(FSLH) // _
+#define _PIPE LS(EQUAL) // |
